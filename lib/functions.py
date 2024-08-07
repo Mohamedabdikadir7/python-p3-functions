a7b3dc2792d92c6be1ff5c70d53019a26d876085
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-08-07 10:19:15 +0300

    python function

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
-
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+greet_programmer()
+
+ 
+
+
+# def greet(name):
+#     print(f"Hello",{name})
+# greet("GUIDO")
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Guido")
+
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("ali")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+result = add(45, 55)
+print(result)
+
 
 def halve(number):
-    pass
+    return number / 2
+
+result = halve(8)
+print(result)
+
+
+
